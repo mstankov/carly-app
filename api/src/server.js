25d1ea32edeeaa030996129a228c6c69c5f672b4
Martin Stankov <martin.stankov@accedia.com>
2019-02-04 16:33:03 +0200

    Added default headers to express server's response

--- api/src/server.js
+++ api/src/server.js
@@ -17,6 +17,15 @@ const init = () => {
         bodyParser.urlencoded({ extended: true })
     );
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    });
+
     server.applyMiddleware({ app });
 
     app.listen({ port: 4000 }, () => {
