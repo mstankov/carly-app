094981a750c94608c88d0a72af25d82ac6b0f07d
Martin Stankov <martin.stankov@accedia.com>
2019-02-05 20:51:22 +0200

    implemented cars query in cars-list component

--- src/app/modules/cars/cars-list/cars-list.component.ts
+++ src/app/modules/cars/cars-list/cars-list.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import Car from '../../../shared/interfaces/car.interface';
 import DataService from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
-import { debug } from 'util';
-
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 @Component({
   selector: 'app-cars-list',
   templateUrl: './cars-list.component.html',
@@ -11,13 +11,40 @@ import { debug } from 'util';
 })
 export class CarsListComponent implements OnInit {
   cars: Observable<Car[]>;
+  loading: Boolean = true;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private apollo: Apollo) { }
 
   ngOnInit() {
-    this.cars = this.dataService.cars;
+    this.apollo
+    .watchQuery({
+      query: query
+    })
+    .valueChanges
+    .subscribe((res: any) => {
+      this.cars = res.data.cars;
+      this.loading = res.data.loading;
+    })
   }
 
   wasSelected = (event: any) => console.log(event);
 
 }
+
+const query = gql`
+  query {
+    cars {
+      manufacturer
+      model
+      topSpeed
+      horsePower
+      torque
+      dateAdded
+      doors
+      yearFrom
+      yearTo
+      information
+      imageUrl
+    }
+  }
+`;
