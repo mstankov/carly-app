05a3efe9039cfd9649904efea4f44ff770bba8a0
Martin Stankov <martin.stankov@accedia.com>
2019-02-05 20:14:46 +0200

    Removed API folder (exported as separate project)

--- api/src/graphql/projection.js
+++
@@ -1,6 +0,0 @@
-export default getProjection = (fieldASTs) => {
-  return fieldASTs.fieldNodes[0].selectionSet.selections.reduce((projections, selection) => {
-    projections[selection.name.value] = true;
-    return projections;
-  }, {});
-}
--- api/src/index.js
+++
@@ -1,6 +0,0 @@
-import { init as server }  from './server';
-import { init as mongodb } from './mongodb';
-
-// initialize MongoDB and Express server with GraphQl
-server();
-mongodb();
